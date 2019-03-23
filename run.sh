131553aefb943d61919fe7297c8ac02225bfe03f
jakezp <jakezp@gmail.com>
2019-03-23 09:57:11 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,18 +12,23 @@ then
 fi
 
 # Populate nut-upsd config files
-cat >/etc/nut/ups.conf <<EOF
+if [[ ! -f /etc/nut/ups.conf ]]; then
+  cat >/etc/nut/ups.conf <<EOF
 [$UPS_NAME]
 	desc = "$UPS_DESC"
 	driver = $UPS_DRIVER
 	port = $UPS_PORT
 EOF
+fi
 
-cat >/etc/nut/upsd.conf <<EOF
+if [[ ! -f /etc/nut/upsd.conf ]]; then
+  cat >/etc/nut/upsd.conf <<EOF
 LISTEN 0.0.0.0 3493
 EOF
+fi
 
-cat >/etc/nut/upsd.users <<EOF
+if [[ ! -f /etc/nut/upsd.users ]]; then
+  cat >/etc/nut/upsd.users <<EOF
 [admin]
 	password = $ADMIN_PASSWORD
 	actions = set
@@ -33,12 +38,15 @@ cat >/etc/nut/upsd.users <<EOF
 	password = $API_PASSWORD
 	upsmon master
 EOF
+fi
 
-cat >/etc/nut/upsmon.conf <<EOF
+if [[ ! -f /etc/nut/upsmon.conf ]]; then
+  cat >/etc/nut/upsmon.conf <<EOF
 MONITOR $UPS_NAME@localhost 1 $API_USER $API_PASSWORD master
 NOTIFYFLAG ONLINE   SYSLOG+WALL
 SHUTDOWNCMD "$SHUTDOWN_CMD"
 EOF
+fi
 
 chgrp -R nut /etc/nut /dev/bus/usb
 chmod -R o-rwx /etc/nut
